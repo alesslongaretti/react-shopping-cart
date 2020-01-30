6013ba4bec72a70713f635b6f76d5c9512d33d91
alesslongaretti <alesslongaretti@gmail.com>
2020-01-30 00:34:35 -0500

    Met MVP

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import data from './data';
 
 // Components
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
@@ -14,27 +15,22 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		setCart(item)
+		setCart([...cart, item])
 		// add the given item to the cart
 	};
 
 	return (
 		<ProductContext.Provider value ={{products, addItem}}>
+			<CartContext.Provider value ={{cart}}>
 			<div className="App">
 				<Navigation cart={cart} />
 
 				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					component={Products}
-				/>
+				<Route exact path="/" component={Products}/>
 
-				<Route
-					path="/cart"
-					component={ShoppingCart}
-				/>
+				<Route path="/cart" component={ShoppingCart}/>
 			</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
